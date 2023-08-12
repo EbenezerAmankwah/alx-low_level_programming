c4de3382e85a40ed9a807116e695f3e252f975e4
EbenezerAmankwah <ebenezeramankwah2004@gmail.com>
2023-08-12 17:44:15 +0000

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("\n");
+	return (0);
+}
