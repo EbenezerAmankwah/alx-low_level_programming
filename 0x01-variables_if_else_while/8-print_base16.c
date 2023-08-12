c6d123742c1f7936f2ec1944838fdaa72f997ceb
EbenezerAmankwah <ebenezeramankwah2004@gmail.com>
2023-08-12 13:25:15 +0000

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,9 +11,9 @@ int main(void)
 	int i;
 	char j;
 
-	for (i = 0 ; i <= 10 ; i++)
+	for (i = 0 ; i < 10 ; i++)
 		putchar(i + '0');
-	for (j = 'a' ; j <= 'g' ; j++)
+	for (j = 'a' ; j <= 'f' ; j++)
 		putchar(j);
 	putchar('\n');
 	return (0);
