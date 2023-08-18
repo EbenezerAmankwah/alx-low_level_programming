fb0a5fbc6a6975153a481bec9527ff6f321b911d
EbenezerAmankwah <ebenezeramankwah2004@gmail.com>
2023-08-18 05:01:48 +0000

    8. You are so much sunshine in every square inch

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -15,11 +15,15 @@ void print_diagonal(int n)
 		int i;
 		int k;
 
-		for (i = 1 ; i <= n ; i++)
+		for (i = 0 ; i < n ; i++)
 		{
 			for (k = 0 ; k <= i ;k++)
-				_putchar(' ');
-			_putchar('\\');
+			{
+				if (k == i)
+					_putchar('\\');
+				else if (k < i)
+					_putchar(' ');
+			}
 			_putchar('\n');
 		}
 	}
