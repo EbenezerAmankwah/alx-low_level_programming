b72dc51f6a231f8d69ff4d2beb4702ad31350b25
EbenezerAmankwah <ebenezeramankwah2004@gmail.com>
2023-08-31 03:40:57 +0000

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: parameter to be checked
+ * Return: square root of n or -1
+ */
+
+int _sqrt_recursion(int n)
+{
+
+}
